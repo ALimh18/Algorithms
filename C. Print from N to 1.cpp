8bce46fd3712a87e6a12747d1704280204c521e2
ALimh18 <154643426+ALimh18@users.noreply.github.com>
2026-03-01 01:44:33 +0200

    sheet 7 recursion Q(C)

---
+++ C. Print from N to 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int n)
+{
+    if(n==0){return;}
+    cout<<n;
+    if (n > 1) cout << " ";
+    return print(n-1);
+}
+
+int main()
+{
+    int x;
+    cin>>x;
+    print(x);
+    return 0;
+}
