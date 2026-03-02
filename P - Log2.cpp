519ddec4f45e293e5a327949d211d3250c053516
ALimh18 <154643426+ALimh18@users.noreply.github.com>
2026-03-02 04:31:49 +0200

    sheet 7 recursion Q(P)

---
+++ P - Log2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int counter=0;
+void log(long long num)
+{
+    if(num==1){return;}
+    counter++;
+    log(num/2);
+
+}
+int main()
+{
+  long long num;
+  cin>>num;
+  log(num);
+  cout<<counter<<endl;
+}
